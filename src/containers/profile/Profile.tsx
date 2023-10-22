fe623d7ee640aa50b413d0205a88549f733899b7
Nguyễn Chánh Đại <ncdai@penphy.edu.vn>
2023-10-22 17:03:20 +0700

    fix(Profile.tsx): remove nickName from the page title to simplify and improve user experience

--- src/containers/profile/Profile.tsx
+++ src/containers/profile/Profile.tsx
@@ -32,7 +32,7 @@ type IProps = {
 };
 
 export const ProfileContainer: React.FC<IProps> = ({ domains }) => {
-  const pageTitle = `${USER.fullName} (${USER.nickName}) | chanhdai.com`;
+  const pageTitle = `${USER.fullName} | chanhdai.com`;
   const shouldShowPhoneNumber = !!USER.phoneNumber;
 
   const handleQuickActionClick = (type: IQuickActionType) => {
