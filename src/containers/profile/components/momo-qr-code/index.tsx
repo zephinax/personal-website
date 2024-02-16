c837199c38a4e78cfa7b4480dc079d3717822c44
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-02-16 18:54:20 +0700

    chore(package.json): update version from 2.10 to 2.11 fix(momo-qr-code): update QR code image import to use the new version of the image refactor(momo-qr-code): use constants for width and height of the QR code image refactor(momo-qr-code): update placeholder attribute to use the constants for width and height of the QR code image

--- src/containers/profile/components/momo-qr-code/index.tsx
+++ src/containers/profile/components/momo-qr-code/index.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
-import QRCode from "@/../public/images/ncdai-momo-qr-code.jpeg";
+import QRCode from "@/../public/images/ncdai-momo-qr-code-v2.jpeg";
+
+const WIDTH = QRCode.width;
+const HEIGHT = QRCode.height;
 
 export const MoMoQRCode = () => {
   return (
@@ -11,7 +14,7 @@ export const MoMoQRCode = () => {
         quality={100}
         priority
         className="select-none"
-        placeholder="data:image/svg+xml,%3Csvg width='755' height='1280' viewBox='0 0 755 1280' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='755' height='1280' fill='white'/%3E%3C/svg%3E%0A"
+        placeholder={`data:image/svg+xml,%3Csvg width='${WIDTH}' height='${HEIGHT}' viewBox='0 0 ${WIDTH} ${HEIGHT}' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='${WIDTH}' height='${HEIGHT}' fill='white'/%3E%3C/svg%3E%0A`}
         sizes="(min-width: 640px) 384px, calc(100vw - 32px)"
       />
     </div>
