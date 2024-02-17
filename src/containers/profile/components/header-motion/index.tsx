520970b0b39bbc7a9ddac25c63586286a93b0164
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-02-17 12:38:01 +0700

    An Tet xong roi, tro lai lam viec binh thuong thoi

--- src/containers/profile/components/header-motion/index.tsx
+++ src/containers/profile/components/header-motion/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { useCallback } from "react";
 
 import { USER } from "../../constants";
 import { IconVerfied } from "../icon-verified";
@@ -13,6 +14,14 @@ export const HeaderMotion = () => {
   const top = useSpring(_top);
   const opacity = useTransform(scrollY, [100, 400], [0.5, 1]);
 
+  const handleHeaderClick = useCallback(() => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, []);
+
   return (
     <motion.div
       className="fixed left-0 right-0 z-50 w-screen"
@@ -21,7 +30,11 @@ export const HeaderMotion = () => {
       <div className="absolute -top-1/2 left-0 -z-10 flex h-full w-full bg-slate-900" />
 
       <div className="mx-auto px-2 pt-2 md:max-w-2xl">
-        <div className="flex h-14 items-center space-x-2 rounded-xl bg-primary-900 px-2 shadow-lg">
+        <div
+          className="flex h-14 cursor-pointer items-center space-x-2 rounded-xl bg-primary-900 px-2 shadow-lg"
+          aria-hidden
+          onClick={handleHeaderClick}
+        >
           <NCDAiAvatar size={40} priority={false} className="rounded-full" />
 
           <div className="flex items-center text-lg font-medium">
