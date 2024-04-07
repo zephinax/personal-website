3313b5a85710d8669307464d0b3b706190b7fcd7
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-04-08 00:19:27 +0700

    fix(route.ts): update API URL to include "/data" path segment for better organization and clarity

--- src/app/apps/gold-price/data/route.ts
+++ src/app/apps/gold-price/data/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const brand = searchParams.get("brand") || "sjc";
 
-  const res = await fetch(`${API_URL}/${brand}`);
+  const res = await fetch(`${API_URL}/data/${brand}`);
   const data = await res.json();
 
   return NextResponse.json(data);
