18d5e0224b482eefccca6877213844e7503fc7d5
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-04-19 22:40:15 +0700

    refactor(page.tsx): update schema to represent a ProfilePage instead of a Person for better semantic accuracy feat(page.tsx): add dateCreated and dateModified fields to ProfilePage schema for better SEO and content freshness feat(page.tsx): add mainEntity field to ProfilePage schema to include detailed user information and social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,52 @@
-import { Person, WithContext } from "schema-dts";
+import dayjs from "dayjs";
+import { ProfilePage, WithContext } from "schema-dts";
 
 import { ProfileContainer } from "@/containers/profile";
 import { USER } from "@/containers/profile/constants";
 
-const getPersonJsonLd = (): WithContext<Person> => {
+const getProfilePageJsonLd = (): WithContext<ProfilePage> => {
   return {
     "@context": "https://schema.org",
-    "@type": "Person",
-    name: USER.fullName,
-    alternateName: USER.nickname,
-    nationality: "Vietnamese",
-    gender: USER.gender,
-    address: {
-      "@type": "PostalAddress",
-      addressLocality: "Binh Thanh District",
-      addressRegion: "Ho Chi Minh City",
-      postalCode: "72300",
-      addressCountry: "Viet Nam",
+    "@type": "ProfilePage",
+    dateCreated: dayjs("2023-10-20 20:10").format(),
+    dateModified: dayjs("2024-04-19 22:36").format(),
+    mainEntity: {
+      "@type": "Person",
+      name: USER.fullName,
+      alternateName: USER.nickname,
+      identifier: USER.username,
+      image: "https://chanhdai.com/images/ncdai-avatar.jpeg",
+      sameAs: [
+        "https://linkedin.com/in/ncdai",
+        "https://github.com/ncdai",
+        "https://www.youtube.com/@ncdai",
+      ],
     },
-    email: USER.email,
-    url: USER.website,
-    jobTitle: "Senior Frontend Developer",
-    sameAs: [
-      "https://linkedin.com/in/ncdai",
-      "https://github.com/ncdai",
-      "https://www.youtube.com/@ncdai",
-    ],
+    // "@type": "Person",
+    // name: USER.fullName,
+    // alternateName: USER.nickname,
+    // nationality: "Vietnamese",
+    // gender: USER.gender,
+    // address: {
+    //   "@type": "PostalAddress",
+    //   addressLocality: "Binh Thanh District",
+    //   addressRegion: "Ho Chi Minh City",
+    //   postalCode: "72300",
+    //   addressCountry: "Viet Nam",
+    // },
+    // email: USER.email,
+    // url: USER.website,
+    // jobTitle: "Senior Frontend Developer",
+    // sameAs: [
+    //   "https://linkedin.com/in/ncdai",
+    //   "https://github.com/ncdai",
+    //   "https://www.youtube.com/@ncdai",
+    // ],
   };
 };
 
 export default function Page() {
-  const websiteJsonLd = getPersonJsonLd();
+  const websiteJsonLd = getProfilePageJsonLd();
 
   return (
     <>
