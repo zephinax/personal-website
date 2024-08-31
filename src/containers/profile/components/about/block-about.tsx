1dc4bf29b089801d2969226b8a8b95abc626d093
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-08-31 15:59:43 +0700

    Update block-about.tsx

--- src/containers/profile/components/about/block-about.tsx
+++ src/containers/profile/components/about/block-about.tsx
@@ -4,7 +4,7 @@ export const BlockAbout = () => {
   return (
     <div className="space-y-2">
       <p>
-        Hello, World! I am Nguyễn Chánh Đại (NCDai), a Senior Frontend Developer
+        Hello, World! I am Nguyễn Chánh Đại (NCDAi), a Senior Frontend Developer
         at{" "}
         <ExternalLink href="https://simplamo.com?ref=IN-926722">
           Simplamo
