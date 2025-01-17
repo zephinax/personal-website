dd4b1c97fa3982674a5f88db5b22a4697ae572f3
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-01-17 15:05:35 +0700

    feat(profile): enhance ChanhDaiCoverGrid with additional styling and dimensions
    Increase the height of elements for better visual balance. Add borders and size indicators to improve UI clarity. Update version to 3.1.12 in package.json to reflect these changes.

--- src/containers/profile/components/chanhdai-cover-grid/index.tsx
+++ src/containers/profile/components/chanhdai-cover-grid/index.tsx
@@ -20,11 +20,20 @@ export const ChanhDaiCoverGrid = () => {
         <Plus className="absolute -bottom-[12.5px] -left-[12.5px]" />
       </div>
 
-      <div className="top-1/2 box-content h-14 w-full -translate-y-1/2 border-y border-zinc-100 dark:border-zinc-900 sm:h-20" />
+      <div className="top-1/2 box-content h-16 w-full -translate-y-1/2 border-y border-zinc-100 dark:border-zinc-900 sm:h-20" />
 
       <div className="flex items-center justify-center text-black dark:text-white">
         <div className="h-full border-r border-zinc-100 dark:border-zinc-900"></div>
-        <ChanhDaiLockup className="h-14 text-black dark:text-white sm:h-20" />
+        <div className="relative">
+          <ChanhDaiLockup className="h-16 text-black dark:text-white sm:h-20" />
+
+          <div className="absolute -bottom-px -left-px -right-px -top-px border border-blue-500 dark:border-zinc-600" />
+          <div className="absolute -left-[5px] -top-[5px] size-[9px] border border-blue-500 bg-background dark:border-zinc-600" />
+          <div className="absolute -right-[5px] -top-[5px] size-[9px] border border-blue-500 bg-background dark:border-zinc-600" />
+          <div className="absolute -bottom-[5px] -left-[5px] size-[9px] border border-blue-500 bg-background dark:border-zinc-600" />
+          <div className="absolute -bottom-[5px] -right-[5px] size-[9px] border border-blue-500 bg-background dark:border-zinc-600" />
+          <div className="absolute -top-[9px] left-1/2 -translate-x-1/2 -translate-y-full transform rounded bg-blue-600 px-1 text-sm text-white after:content-['128_x_64'] dark:bg-zinc-600 sm:after:content-['160_x_80']" />
+        </div>
         <div className="h-full border-r border-zinc-100 dark:border-zinc-900"></div>
       </div>
     </div>
