5f86a8140e085f4b8784a8827b91400be8ee5f9b
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-01-29 22:11:35 +0700

    refactor(header): restructure header component layout for better readability and maintainability fix(header): adjust CSS classes to ensure consistent styling across light and dark modes chore(package.json): bump version from 2025.1 to 2025.1.1 to reflect non-breaking changes in the codebase

--- src/features/profile/components/header/index.tsx
+++ src/features/profile/components/header/index.tsx
@@ -10,31 +10,42 @@ export const Header = () => {
     <header className="mt-2">
       <ChanhDaiCoverGrid />
 
-      <div className="after:bg-grid border-grid relative flex items-end border-x after:absolute after:bottom-0 after:-left-[100vw] after:h-px after:w-[200vw]">
+      <div className="after:bg-grid border-grid relative flex border-x after:absolute after:bottom-0 after:-left-[100vw] after:h-px after:w-[200vw]">
         <div className="border-grid border-r">
           <div className="relative z-1 mx-0.5 my-[3px]">
             <ChanhDaiAvatar className="ring-border ring-offset-background size-32 rounded-full ring-1 ring-offset-2 sm:size-40" />
           </div>
         </div>
 
-        <div className="border-grid flex-1 border-t pl-4">
-          <h1 className="font-heading mb-1 flex items-center text-3xl font-semibold">
-            {USER.displayName}
-            &nbsp;
-            <QuaricTooltip content="Verified account">
-              <span
-                aria-hidden
-                aria-label="Verified account"
-                className="text-info-foreground relative top-px cursor-pointer"
-              >
-                <IconVerified size="0.6em" />
-              </span>
-            </QuaricTooltip>
-          </h1>
+        <div className="flex flex-1 flex-col">
+          <div className="flex grow items-end pb-1 pl-4">
+            <div className="hidden font-mono text-xs whitespace-pre text-zinc-300 select-none sm:block dark:text-zinc-800">
+              {"text-3xl "}
+              <span className="inline dark:hidden">text-zinc-950</span>
+              <span className="hidden dark:inline">text-zinc-50</span>
+              {" font-semibold"}
+            </div>
+          </div>
 
-          <p className="text-muted-foreground mb-1 font-mono text-sm text-balance">
-            {USER.bio}
-          </p>
+          <div className="border-grid border-t">
+            <h1 className="font-heading flex items-center pl-4 text-3xl font-semibold">
+              {USER.displayName}
+              &nbsp;
+              <QuaricTooltip content="Verified account">
+                <span
+                  aria-hidden
+                  aria-label="Verified account"
+                  className="text-info-foreground relative top-px cursor-pointer"
+                >
+                  <IconVerified size="0.6em" />
+                </span>
+              </QuaricTooltip>
+            </h1>
+
+            <p className="text-muted-foreground border-grid border-t py-1 pl-4 font-mono text-sm text-balance">
+              {USER.bio}
+            </p>
+          </div>
         </div>
       </div>
     </header>
