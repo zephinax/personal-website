a624ad0670241510a1fd98cbcf5f0a6580c89778
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-02-08 21:35:39 +0700

    style(tooltip.tsx): update dark mode border and background colors for better contrast and consistency

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -21,7 +21,7 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-lg border border-zinc-950 bg-zinc-900 px-3 py-1.5 text-base text-zinc-50 shadow-md dark:border-zinc-600 dark:bg-zinc-700",
+      "animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-lg border border-zinc-950 bg-zinc-900 px-3 py-1.5 text-base text-zinc-50 shadow-md dark:border-zinc-700 dark:bg-zinc-800",
       className
     )}
     {...props}
