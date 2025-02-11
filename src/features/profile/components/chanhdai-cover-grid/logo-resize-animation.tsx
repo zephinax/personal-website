f3b2e6ab204189539f677dd8f00afbd3b851ed9e
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-02-11 20:19:18 +0700

    feat(logo-resize-animation.tsx): add fade-out animation on completion to enhance visual feedback fix(logo-resize-animation.tsx): adjust transition duration for smoother animation

--- src/features/profile/components/chanhdai-cover-grid/logo-resize-animation.tsx
+++ src/features/profile/components/chanhdai-cover-grid/logo-resize-animation.tsx
@@ -5,6 +5,15 @@ import { useState } from "react";
 
 import { ChanhDaiWordmark } from "@/components/brand/chanhdai-wordmark";
 
+const variants = {
+  show: {
+    opacity: 1,
+  },
+  hide: {
+    opacity: 0,
+  },
+};
+
 type IProps = {
   minWidth?: number;
   maxWidth?: number;
@@ -15,6 +24,7 @@ export const LogoResizeAnimation = ({
   maxWidth = 160,
 }: IProps) => {
   const [width, setWidth] = useState(maxWidth);
+  const [isEnd, setIsEnd] = useState(false);
 
   return (
     <motion.div
@@ -22,21 +32,32 @@ export const LogoResizeAnimation = ({
       initial={{ width: maxWidth }}
       whileInView={{ width: [maxWidth, minWidth, maxWidth] }}
       viewport={{ once: true }}
-      transition={{ duration: 0.8, delay: 0.4 }}
+      transition={{ duration: 1, delay: 0.4 }}
       onUpdate={(latest) => {
         setWidth(latest.width as number);
       }}
+      onAnimationComplete={() => {
+        setIsEnd(true);
+      }}
     >
-      <div className="*:bg-background absolute -top-px -right-px -bottom-px -left-px border border-blue-500 *:absolute *:size-[9px] *:border *:border-blue-500 dark:border-zinc-600 dark:*:border-zinc-600">
+      <motion.div
+        variants={variants}
+        animate={isEnd ? "hide" : "show"}
+        className="*:bg-background absolute -top-px -right-px -bottom-px -left-px border border-blue-500 *:absolute *:size-[9px] *:border *:border-blue-500 dark:border-zinc-600 dark:*:border-zinc-600"
+      >
         <div className="-top-[5px] -left-[5px]" />
         <div className="-top-[5px] -right-[5px]" />
         <div className="-bottom-[5px] -left-[5px]" />
         <div className="-right-[5px] -bottom-[5px]" />
-      </div>
+      </motion.div>
 
-      <div className="absolute -top-[9px] left-1/2 -translate-x-1/2 -translate-y-full transform rounded-sm bg-blue-600 px-1 text-sm whitespace-nowrap text-white dark:bg-zinc-600">
+      <motion.div
+        variants={variants}
+        animate={isEnd ? "hide" : "show"}
+        className="absolute -top-[9px] left-1/2 -translate-x-1/2 -translate-y-full transform rounded-sm bg-blue-600 px-1 text-sm whitespace-nowrap text-white dark:bg-zinc-600"
+      >
         {Math.round(width)} x {Math.round(width / 2)}
-      </div>
+      </motion.div>
 
       <ChanhDaiWordmark className="size-full text-black dark:text-white" />
     </motion.div>
