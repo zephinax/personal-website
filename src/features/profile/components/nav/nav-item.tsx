9e42a14ae5c22ac8c77c0ab66b8d16c9d2c247d5
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-04-25 23:48:11 +0700

    refactor(nav-item.tsx): remove motion animations and simplify component structure for performance improvement style(nav-item.tsx): update class names for consistent styling and readability style(nav.tsx): simplify class names for better maintainability and consistency

--- src/features/profile/components/nav/nav-item.tsx
+++ src/features/profile/components/nav/nav-item.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { motion } from "motion/react";
-
 import { cn } from "@/lib/cn";
 
 import { NavLink } from "./nav-link";
@@ -13,24 +9,12 @@ export function NavItem({
   active?: boolean;
 }) {
   return (
-    <div className="relative">
-      <NavLink
-        className={cn(
-          "relative z-1 font-medium transition-all duration-500",
-          active && "text-zinc-700 dark:!text-zinc-50 dark:text-shadow-xs"
-        )}
-        {...props}
-      />
-
-      {active && (
-        <motion.div
-          className="absolute -inset-x-1.5 -inset-y-1 rounded-lg border border-white bg-zinc-50 shadow ring ring-zinc-200 dark:border-0 dark:bg-accent/50 dark:shadow-none dark:ring-0"
-          layoutId="nav-item-active"
-          transition={{ duration: 0.5 }}
-        >
-          <div className="hidden h-2 w-full translate-y-3 bg-white blur-lg filter dark:block" />
-        </motion.div>
+    <NavLink
+      className={cn(
+        "font-mono text-sm font-medium text-muted-foreground transition-all duration-300",
+        active && "text-foreground"
       )}
-    </div>
+      {...props}
+    />
   );
 }
