db1e18101417c71177efb3572adc1073b80439b1
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-04-26 17:52:33 +0700

    style(layout.tsx): replace en dash with hyphen in default title for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   },
   title: {
     template: `%s | ${SITE_INFO.name}`,
-    default: `${USER.displayName} – ${USER.jobTitle}`,
+    default: `${USER.displayName} - ${USER.jobTitle}`,
   },
   description: SITE_INFO.description,
   keywords: SITE_INFO.keywords,
