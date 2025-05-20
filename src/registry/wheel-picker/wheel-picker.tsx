702f8cc603726a84e7f76fe732079ce691927630
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-05-21 00:51:03 +0700

    style(wheel-picker.tsx): update styles for improved UI consistency and appearance in dark mode
    - Adjust border and shadow styles for better visual hierarchy
    - Modify dark mode text and background colors for better contrast
    - Add rounded corners to highlight wrapper for a polished look

--- src/registry/wheel-picker/wheel-picker.tsx
+++ src/registry/wheel-picker/wheel-picker.tsx
@@ -14,7 +14,9 @@ function WheelPickerWrapper({
   return (
     <WheelPickerPrimitive.WheelPickerWrapper
       className={cn(
-        "max-w-56 rounded-lg border border-zinc-200 bg-white shadow-xs dark:border-zinc-800 dark:bg-zinc-950",
+        "max-w-56 rounded-lg bg-white px-1 shadow-sm ring ring-black/5 dark:bg-zinc-900 dark:ring-white/15",
+        "[&>[data-rwp]]:first:[&>[data-rwp-highlight-wrapper]]:rounded-s-md",
+        "[&>[data-rwp]]:last:[&>[data-rwp-highlight-wrapper]]:rounded-e-md",
         className
       )}
       {...props}
@@ -29,9 +31,9 @@ function WheelPicker({
   return (
     <WheelPickerPrimitive.WheelPicker
       classNames={{
-        optionItem: "text-zinc-400 dark:text-zinc-600",
+        optionItem: "text-zinc-400 dark:text-zinc-500",
         highlightWrapper:
-          "bg-zinc-100 text-zinc-950 dark:bg-zinc-900 dark:text-zinc-50",
+          "bg-zinc-100 text-zinc-950 dark:bg-zinc-800 dark:text-zinc-50",
         ...classNames,
       }}
       {...props}
