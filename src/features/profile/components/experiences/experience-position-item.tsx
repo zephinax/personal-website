19ceecd0902b31108a3df224373d589b4d2879c3
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-06-16 15:17:22 +0700

    refactor(icons): replace ChevronDownIcon with PlusIcon for consistency style: update icon rotation logic for open state in collapsible components fix: remove unnecessary data-state attribute from AccordionPrimitive.Trigger components

--- src/features/profile/components/experiences/experience-position-item.tsx
+++ src/features/profile/components/experiences/experience-position-item.tsx
@@ -17,7 +17,7 @@ export function ExperiencePositionItem({
   return (
     <AccordionPrimitive.Item value={position.id} asChild>
       <div className="relative last:before:absolute last:before:h-full last:before:w-4 last:before:bg-background">
-        <AccordionPrimitive.Trigger className="group/experience block w-full text-left select-none [&[data-state=open]_.lucide-plus]:rotate-45">
+        <AccordionPrimitive.Trigger className="group/experience block w-full text-left select-none">
           <div className="relative z-1 mb-1 flex items-center gap-3 bg-background">
             <div className="flex size-6 shrink-0 items-center justify-center rounded-lg bg-muted text-muted-foreground">
               <ExperienceIcon className="size-4" icon={position.icon} />
@@ -28,7 +28,7 @@ export function ExperiencePositionItem({
             </h4>
 
             <PlusIcon
-              className="size-4 shrink-0 text-muted-foreground transition-transform duration-300"
+              className="size-4 shrink-0 text-muted-foreground transition-transform duration-300 group-data-[state=open]/experience:rotate-45"
               strokeWidth={2.5}
             />
           </div>
