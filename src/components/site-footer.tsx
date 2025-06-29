4e06fab51ee0b31a880822e40eeaf074fdc5e017
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-06-29 09:35:17 +0700

    style(site-footer.tsx): add padding to paragraphs for improved layout consistency

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -9,11 +9,11 @@ export function SiteFooter() {
   return (
     <footer className="max-w-screen overflow-x-hidden px-2">
       <div className="screen-line-before mx-auto border-x border-edge pt-4 md:max-w-3xl">
-        <p className="mb-1 text-center font-mono text-sm text-balance text-muted-foreground">
+        <p className="mb-1 px-4 text-center font-mono text-sm text-balance text-muted-foreground">
           Inspired by tailwindcss.com + ui.shadcn.com
         </p>
 
-        <p className="mb-4 text-center font-mono text-sm text-balance text-muted-foreground">
+        <p className="mb-4 px-4 text-center font-mono text-sm text-balance text-muted-foreground">
           Built by{" "}
           <a
             className="link"
