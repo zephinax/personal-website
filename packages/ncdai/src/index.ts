a798e7ac4aa5d5d132c1c50aff1fe5e0c42831a7
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-10-05 00:04:03 +0700

    feat(ncdai): implement component addition functionality using spawnSync
    fix(ncdai): correct usage message to reflect correct CLI name
    
    chore(ncdai): update package version to 0.0.1 for initial release
    
    docs(ncdai): add repository and bugs information to package.json for better project tracking and issue reporting

--- packages/ncdai/src/index.ts
+++ packages/ncdai/src/index.ts
@@ -2,12 +2,12 @@
 
 // Thanks https://github.com/haydenbleasel/kibo/blob/main/scripts/index.ts
 
-// import { spawnSync } from "node:child_process";
+import { spawnSync } from "node:child_process";
 
 const args = process.argv.slice(2);
 
 if (args.length < 2 || args[0] !== "add") {
-  console.log("Usage: npx kibo-ui add [...packages]");
+  console.log("Usage: npx ncdai add [...packages]");
   process.exit(1);
 }
 
@@ -20,22 +20,20 @@ for (const packageName of packageNames) {
 
   console.log(`Adding ${packageName} component...`);
 
-  process.exit(0);
+  const url = new URL(`r/${packageName}.json`, "https://chanhdai.com");
 
-  // const url = new URL(`r/${packageName}.json`, "https://chanhdai.com");
+  const command = `npx -y shadcn@latest add ${url.toString()}`;
 
-  // const command = `npx -y shadcn@latest add ${url.toString()}`;
+  const result = spawnSync(command, {
+    stdio: "inherit",
+    shell: true,
+  });
 
-  // const result = spawnSync(command, {
-  //   stdio: "inherit",
-  //   shell: true,
-  // });
-
-  // if (result.error) {
-  //   console.error(`Failed to add ${packageName}:`, result.error.message);
-  //   process.exit(1);
-  // } else if (result.status !== 0) {
-  //   console.error(`Command failed with exit code ${result.status}`);
-  //   process.exit(1);
-  // }
+  if (result.error) {
+    console.error(`Failed to add ${packageName}:`, result.error.message);
+    process.exit(1);
+  } else if (result.status !== 0) {
+    console.error(`Command failed with exit code ${result.status}`);
+    process.exit(1);
+  }
 }
