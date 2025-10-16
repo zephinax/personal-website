6273440850b703793675a4922fc645e4d907f391
Zephinax <zephinax@gmail.com>
2025-10-16 13:40:29 +0330

    chore: update experiance

--- src/features/profile/data/experiences.ts
+++ src/features/profile/data/experiences.ts
@@ -38,61 +38,39 @@ export const EXPERIENCES: Experience[] = [
       },
     ],
   },
+  {
+    id: "teamiran",
+    companyName: "IRAN FGC",
+    companyLogo: "/images/companies/teamiran.webp",
+    positions: [
+      {
+        id: "20f8bfe5-b6a3-4b0d-ac2f-6fccd50d417e",
+        title: "Project Manager",
+        employmentPeriod: {
+          start: "06.2024",
+          end: "09.2024",
+        },
+        employmentType: "Full-time",
+        icon: "code",
+        description: `- **Led project planning and execution** for multiple Team Iran FGC initiatives.
+        - **Defined scopes, KPIs, and timelines** aligned with business objectives.
+        - Oversaw **risk management, resources, and milestones**.
+        - Guided **technical and product decisions** balancing innovation and feasibility.
+        - Reported progress to **executive leadership**, promoting accountability and innovation.`,
+        skills: [
+          "Project Management",
+          "Leadership",
+          "Technical Analysis",
+          "Problem-solving",
+          "Product Strategy",
+        ],
+        isExpanded: false,
+      },
+    ],
+    isCurrentEmployer: false,
+  },
 ];
 
-// {
-//   id: "simplamo",
-//   companyName: "Simplamo Enterprise JSC",
-//   companyLogo: "https://assets.chanhdai.com/images/companies/simplamo.webp",
-//   positions: [
-//     {
-//       id: "20f8bfe5-b6a3-4b0d-ac2f-6fccd50d417e",
-//       title: "Senior Frontend Developer",
-//       employmentPeriod: {
-//         start: "10.2022",
-//       },
-//       employmentType: "Full-time",
-//       icon: "code",
-//       description: `- Develop [AI Chat](https://help.simplamo.com/features/simplamo-ai/ai-chat/guide_simplamo_ai_chat?ref=IN-926722) and [AI Assistant](https://help.simplamo.com/features/simplamo-ai/ai-expert/aiexpert-rockdiscribe?ref=IN-926722) features.
-// - Develop [Whiteboards](https://help.simplamo.com/features/whiteboard/overview?ref=IN-926722) with real-time collaboration.
-// - Build and maintain the [Zalo Mini App](https://zalo.me/s/1736112917405511258/) for Simplamo with seamless integration.
-// - Develop interactive chart and analytics widgets for the [Dashboard](https://help.simplamo.com/features/dashboard/overview) to enhance data visualization.
-// - Develop and maintain core features to enhance functionality and user experience.
-// - Ensure UI/UX consistency and adherence to standards.
-// - Implement robust frontend solutions for web and mobile platforms.
-// - Analyze technical capabilities and provide optimal solutions.`,
-//       skills: [
-//         "TypeScript",
-//         "Next.js",
-//         "React Native",
-//         "MobX",
-//         "MobX-State-Tree",
-//         "Tailwind CSS",
-//         "Dify",
-//         "Zalo Mini App",
-//         "Agile",
-//         "Teamwork",
-//         "Research",
-//         "Problem-solving",
-//       ],
-//       isExpanded: true,
-//     },
-//     {
-//       id: "cedd7adb-4118-4085-9983-ae00530b49e2",
-//       title: "UI Design Lead",
-//       employmentPeriod: {
-//         start: "10.2022",
-//       },
-//       employmentType: "Full-time",
-//       icon: "design",
-//       description: `- Ensure UI/UX consistency and high-quality standards.
-// - Design intuitive, user-focused interfaces aligned with business goals.
-// - Define and establish a cohesive UI style for Simplamo.`,
-//       skills: ["Creativity", "UI/UX Design", "Figma"],
-//     },
-//   ],
-//   isCurrentEmployer: true,
-// },
 // {
 //   id: "quaric",
 //   companyName: "Quaric Co., Ltd.",
