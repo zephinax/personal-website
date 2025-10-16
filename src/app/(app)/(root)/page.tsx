5e7732fa7d34255e306e64ebb53268acae9c510a
Zephinax <zephinax@gmail.com>
2025-10-16 13:57:09 +0330

    chore: update awards

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -5,7 +5,7 @@ import { About } from "@/features/profile/components/about";
 import { Awards } from "@/features/profile/components/awards";
 import { Blog } from "@/features/profile/components/blog";
 import { Brand } from "@/features/profile/components/brand";
-import { Certifications } from "@/features/profile/components/certifications";
+// import { Certifications } from "@/features/profile/components/certifications";
 import { Experiences } from "@/features/profile/components/experiences";
 import { GitHubContributions } from "@/features/profile/components/github-contributions";
 import { Overview } from "@/features/profile/components/overview";
@@ -59,7 +59,7 @@ export default function Page() {
         <Awards />
         <Separator />
 
-        <Certifications />
+        {/*<Certifications />*/}
         <Separator />
 
         <Brand />
