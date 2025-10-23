de99f08a535b480735bb5031d9ec6ecffc97a2f5
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-10-23 22:40:37 +0700

    refactor(scroll-top.tsx, button.tsx): update scroll behavior for smoother experience and adjust link button style

--- src/components/scroll-top.tsx
+++ src/components/scroll-top.tsx
@@ -36,7 +36,7 @@ export function ScrollTop({
       )}
       variant="secondary"
       size="icon-lg"
-      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      onClick={() => window.scrollTo({ top: 0 })}
       {...props}
     >
       <ArrowUpIcon className="size-5" />
