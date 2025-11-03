c9e66977cfdfb22c092af5fbc88441a7cf3e6a89
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-11-03 22:18:26 +0700

    feat(icons.tsx): add new 'animationsdev' icon for use in UI components
    feat(certifications.ts): add new certification 'Animations on the Web' to profile data

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -2,6 +2,15 @@ import type { Certification } from "../types/certifications";
 
 export const CERTIFICATIONS: Certification[] = [
   {
+    title: "Animations on the Web",
+    issuer: "animations.dev",
+    issuerIconName: "animationsdev",
+    issueDate: "2025-11-03",
+    credentialID: "dfb419c8-5ed7-43f2-b60c-446bc50a39b1",
+    credentialURL:
+      "https://animations.dev/certificate/dfb419c8-5ed7-43f2-b60c-446bc50a39b1",
+  },
+  {
     title: "Certificate of Trademark Registration No. 565092",
     issuer: "Intellectual Property Office of Viet Nam",
     issuerLogoURL:
