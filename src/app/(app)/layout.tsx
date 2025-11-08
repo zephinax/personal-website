6878e7255f0e18faba9d80bd801244d911a00d4d
Zephinax <zephinax@gmail.com>
2025-11-08 23:40:58 +0330

    add url

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import Script from "next/script";
 
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
@@ -10,6 +11,11 @@ const ScrollTop = dynamic(() =>
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Script
+        src="https://rybbit.elkids.ir/api/script.js"
+        data-site-id="1"
+        strategy="afterInteractive"
+      />
       <SiteHeader />
       <main className="max-w-screen overflow-x-hidden px-2">{children}</main>
       <SiteFooter />
