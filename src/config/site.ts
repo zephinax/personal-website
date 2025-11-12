0e651107127e16e18870ca93d165ce9fa9466642
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-11-12 22:28:23 +0700

    fix(site.ts): update UTM parameters for accurate tracking

--- src/config/site.ts
+++ src/config/site.ts
@@ -35,6 +35,6 @@ export const SOURCE_CODE_GITHUB_URL = "https://github.com/ncdai/chanhdai.com";
 
 export const UTM_PARAMS = {
   utm_source: "chanhdai.com",
-  utm_medium: "portfolio_website",
-  utm_campaign: "referral",
+  utm_medium: "referral",
+  utm_campaign: "portfolio",
 };
